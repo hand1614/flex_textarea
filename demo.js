3d84291a4dd9eea026e66026c98d2f5e9d046810
6218u <furaigonn520@gmail.com>
2023-05-31 21:16:21 +0900

    .

---
+++ demo.js
@@ -0,0 +1,6 @@
+
+"use strict";
+
+import { textarea_resize } from "https://hand1614.github.io/flex_textarea/flex_textarea.js" ;
+
+document.querySelector( "textarea.flex_textarea" ).addEventListener( "input", textarea_resize ) ;
