11f24b15e263ba5191bdd4075c67110dbff3bbe0
hand1614 <94950618+hand1614@users.noreply.github.com>
2023-05-31 22:57:27 +0900

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,7 @@
 
 "use strict";
 
-import { textarea_resize } from "https://hand1614.github.io/flex_textarea/flex_textarea.js" ;
+// import { textarea_resize } from "https://hand1614.github.io/flex_textarea/flex_textarea.js" ;
+import { textarea_resize } from "./flex_textarea.js" ;
 
 document.querySelector( "textarea.flex_textarea" ).addEventListener( "input", textarea_resize ) ;
