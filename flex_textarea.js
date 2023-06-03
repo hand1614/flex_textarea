c6b9f9124830b045e53c5af135c6895d45f0a6fa
6218u <furaigonn520@gmail.com>
2023-06-03 14:44:16 +0900

    .

--- flex_textarea.js
+++ flex_textarea.js
@@ -5,8 +5,9 @@ export function textarea_resize ( event ) {
   const padding  = style.padding ;
   const min_h    = parseFloat( style.minHeight ) + parseFloat( style.paddingTop )  + parseFloat( style.paddingBottom ) + 1 ;
   const min_w    = parseFloat( style.minWidth )  + parseFloat( style.paddingLeft ) + parseFloat( style.paddingRight )  + 1 ;
-  const border_h = style.borderTopWidth  + style.borderBottomWidth ;
-  const border_w = style.borderLeftWidth + style.borderRightWidth  ;
+  const border_h = parseFloat( style.borderTopWidth )  + parseFloat( style.borderBottomWidth ) ;
+  const border_w = parseFloat( style.borderLeftWidth ) + parseFloat( style.borderRightWidth )  ;
+  console.log( min_h, min_w, this.scrollHeight, this.scrollWidth ) ; // 先にパディングを0にしないとパディング領域に溢れた分が計算されない
   if( ( this.scrollWidth < min_w ) && ( this.scrollHeight < min_h ) ) return ;
   this.parentElement.style.cssText = `height: ${ this.scrollHeight + border_h }px ; width: ${ this.scrollWidth + border_w }px ; ` ;
   this.style.cssText =               `height: 0px ; width: 0px ; padding: 0px ; ` ;
